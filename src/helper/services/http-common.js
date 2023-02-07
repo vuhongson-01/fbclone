4e64518766d7f461d18815aa26b4e445638915ae
Macbook-Pro-cua-Vu-2 <vuhongsonchv1619@gmail.com>
2023-02-07 08:40:15 +0700

    v1

--- src/helper/services/http-common.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-import {BASE_URL} from '../../constants/constants';
-
-export default axios.create({
-  baseURL: BASE_URL,
-  headers: {
-    'Content-type': 'application/json',
-  },
-});
